407756dd38f9a1a9b44e5084c482f380f1f2e309
Leopold Mauersberger <leopold.mauersberger@gmail.com>
2024-12-31 17:31:12 +0100

    Add remapping of ADJ Accu Scan 250 to 4 Channels

--- main/main.c
+++ main/main.c
@@ -13,7 +13,8 @@
 
 static const char *TAG = "main";
 
-static uint8_t data[DMX_PACKET_SIZE] = {};  // Buffer to store DMX data
+static uint8_t data_in[DMX_PACKET_SIZE] = {};  // Buffer to store DMX in data
+static uint8_t data_out[DMX_PACKET_SIZE] = {};  // Buffer to store DMX out data
 
 void app_main() {
   const dmx_port_t dmx_num_receive = DMX_NUM_1;
@@ -35,21 +36,31 @@ void app_main() {
     if (dmx_receive(dmx_num_receive, &packet, DMX_TIMEOUT_TICK)) {
       // ESP_LOGI(TAG, "DMX received.");
 
-      dmx_read(dmx_num_receive, data, DMX_PACKET_SIZE);
+      dmx_read(dmx_num_receive, data_in, DMX_PACKET_SIZE);
       // ESP_LOGI(TAG, "Start code: %02x, Size: %i",
       //          packet.sc, packet.size);
       // ESP_LOG_BUFFER_HEX(TAG, data, 16);  // Log first 16 bytes
 
+      // ADJ ACCU Scan 250 at Address 1-8
+      data_out[1] = data_in[1]; // Pan
+      data_out[2] = data_in[2]; // Tilt
+      data_out[3] = data_in[3]; // Color
+      data_out[4] = 0; // Gobo - Fix Spot
+      data_out[5] = 0; // Gobo Rotation - No Rotation
+      data_out[6] = 32 + data_in[4]/8; // Shutter/Storbe - Only use dimmer range
+      data_out[7] = 0;  // Reset/internal programs - Normal color change
+      data_out[8] = 0; // Speed of pan/tilt movement - Max speed
+
       // Send data and block until it's sent
       dmx_send_num(dmx_num_send, DMX_PACKET_SIZE);
       dmx_wait_sent(dmx_num_send, DMX_TIMEOUT_TICK);
-      dmx_write(dmx_num_send, data, DMX_PACKET_SIZE);
+      dmx_write(dmx_num_send, data_out, DMX_PACKET_SIZE);
       // ESP_LOGI(TAG, "Sent first DMX value 0x%02x", data[1]);
     } else {
       // DMX timed out after having been previously connected
       // ESP_LOGI(TAG, "No DMX reveived.");
     }
-    // Only send a packet every ms
+    // Only send a packet every 100 ms
     vTaskDelayUntil(&now, pdMS_TO_TICKS(100));
   }
 }
