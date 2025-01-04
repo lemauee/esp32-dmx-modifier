67ae2cfca6a568b9e0c83487f60229e5e66a78e5
Leopold Mauersberger <leopold.mauersberger@gmail.com>
2025-01-04 23:41:30 +0100

    Implement fire better.

--- main/main.c
+++ main/main.c
@@ -17,6 +17,11 @@ static const char *TAG = "main";
 static uint8_t data_in[DMX_PACKET_SIZE] = {};  // Buffer to store DMX in data
 static uint8_t data_out[DMX_PACKET_SIZE] = {};  // Buffer to store DMX out data
 
+int16_t clamp_i(int16_t num, int16_t min, int16_t max){
+  const float t = num < min ? min : num;
+  return t > max ? max : t;
+}
+
 void app_main() {
   const dmx_port_t dmx_num_receive = DMX_NUM_1;
   const dmx_port_t dmx_num_send = DMX_NUM_0;
@@ -30,9 +35,28 @@ void app_main() {
 
   dmx_packet_t packet;
 
-  while (1) {
-    TickType_t now = xTaskGetTickCount();
+  // Constants
+  const int16_t fire_change_divider_red = 3; 
+  const uint8_t fire_red_min = 50;
+  const uint8_t fire_red_max = 255;
+  const uint8_t fire_red_start = ((uint16_t)fire_red_min + (uint16_t)fire_red_max)/2;
+  const int16_t fire_change_divider_green = 10; 
+  const uint8_t fire_green_min = 10;
+  const uint8_t fire_green_max = 50;
+  const uint8_t fire_green_start = ((uint16_t)fire_green_min + (uint16_t)fire_green_max)/2;
+  const int16_t fire_change_divider_blue = 20;
+  const uint8_t fire_blue_min = 0;
+  const uint8_t fire_blue_max = 0;
+  const uint8_t fire_blue_start = ((uint16_t)fire_blue_min + (uint16_t)fire_blue_max)/2;
+  const uint32_t fire_change_interval_ms = 400;
 
+  // State variables
+  int16_t fire_red = fire_red_start;
+  int16_t fire_green = fire_green_start;
+  int16_t fire_blue = fire_blue_start;
+  TickType_t fire_last_change = xTaskGetTickCount();
+
+  while (1) {
     // Block until a packet is received
     if (dmx_receive(dmx_num_receive, &packet, DMX_TIMEOUT_TICK)) {
       // ESP_LOGI(TAG, "DMX received.");
@@ -102,13 +126,24 @@ void app_main() {
 
       // Flatbeam Duo 
       // Lagerfeuer
+      TickType_t now = xTaskGetTickCount();
+      if((now - fire_last_change) > pdMS_TO_TICKS(fire_change_interval_ms)){
+        fire_last_change = now;
+        int8_t random_red = esp_random();
+        int8_t random_green = esp_random();
+        int8_t random_blue = esp_random();
+        fire_red = clamp_i(fire_red + random_red/fire_change_divider_red,fire_red_min,fire_red_max);
+        fire_green = clamp_i(fire_green + random_green/fire_change_divider_green,fire_green_min,fire_green_max);
+        fire_blue = clamp_i(fire_blue + random_blue/fire_change_divider_blue,fire_blue_min,fire_blue_max);
+      }
+
       float scaling = ((float)(data_in[16]))/((float)255.F);
-      data_out[100] = (uint8_t)((float)(64 + ((uint8_t)esp_random())/2)*scaling); // red
-      data_out[101] = (uint8_t)((float)(64 + ((uint8_t)esp_random())/2)*scaling); // green
-      data_out[102] = (uint8_t)((float)(64 + ((uint8_t)esp_random())/2)*scaling); // blue
+      data_out[100] = (uint8_t)(((float)fire_red)*scaling); // red
+      data_out[101] = (uint8_t)(((float)fire_green)*scaling); // green
+      data_out[102] = (uint8_t)(((float)fire_blue)*scaling); // blue
 
       // White
-      data_out[103] = data_in[16]; // white
+      data_out[103] = data_in[15]; // white
 
       // Send data and block until it's sent
       dmx_send_num(dmx_num_send, DMX_PACKET_SIZE);
